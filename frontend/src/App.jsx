0312a6e6572789bb80f5856c531edc60e203e08f
nitish05584 <devnath8471@gmail.com>
2026-04-12 14:26:12 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      
+      <Route path='/' element={<Home/>}/>
+
+      <Route path='/login' element={<Login/>}/>
+
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+     
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { ShoppingCart } from 'lucide-react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+    const navigate=useNavigate()
+  return (
+    <div className='z-50 sticky top-0 bg-background/50 border-b backdrop-blur'>
+     <div className='container mx-auto px-6 py-4 flex flex-col sm:flex-row items-center justify-between'>
+
+        <h1 className='text-2xl font-bold'>QuickCart</h1>
+     <ul className='flex justify-center items-center space-x-6'>
+      <li className='cursor-pointer'onClick={()=>navigate("/")}>Home</li>  
+
+       <li className='cursor-pointer'onClick={()=>navigate("/products")}>Products</li>
+
+        <li className='cursor-pointer relative flex items-center'onClick={()=>navigate("/cart")}><ShoppingCart className='w-6 h-6'/>
+        <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full'>5</span> 
+        </li> 
+        
+            
+        </ul>
+        </div>   
+      
+    </div>
+  )
+}
+
+export default Navbar
