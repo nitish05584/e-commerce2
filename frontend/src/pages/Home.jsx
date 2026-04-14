4d8d5d6a66223c6846c30edf904f7b49baaa504f
nitish05584 <devnath8471@gmail.com>
2026-04-14 16:02:05 +0530

    Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import Hero from '@/components/Hero'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate=useNavigate()
   return (
     <div>
-      <Hero/>
+      <Hero navigate={navigate}/>
+      <div className='top products mt-4 p-4'>
+        <h1 className='text-3xl mb-4'>Latest Products</h1>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
+          
+        </div>
+      </div>
     </div>
   )
 }
