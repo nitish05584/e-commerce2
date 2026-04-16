ec36836b6cbc127674ce2651e507a479af54b67c
nitish05584 <nitish05584@users.noreply.github.com>
2026-04-16 23:56:50 +0530

    order

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -4,6 +4,11 @@ const Cart = require("../models/Cart");
 const Order = require("../models/Order");
 const { create } = require("../models/Otp");
 const Product = require("../models/Product");
+const dotenv=require("dotenv")
+dotenv.config()
+
+
+const Stripe=require("stripe")
 
 
 const newOrderCode=async(req,res)=>{
@@ -135,4 +140,118 @@ const getStats=async(req,res)=>{
 }
 
 
-module.exports={newOrderCode,getAllOrders,getAllOrdersAdmin,getMyOrder,updateStatus,getStats}
+const stripe=new Stripe(process.env.Stripe_Secret_Key)
+
+const newOrderOnline=async(req,res)=>{
+    try {
+        const {method,phone,address} = req.body;
+        const cart=await Cart.find({user:req.user._id}).populate("products")
+
+        if(!cart.length){
+            return res.status(400).json({message:"Cart is empty"})
+        }
+        const subTotal=cart.reduce((total,item)=>total+item.product.price*item.quantity,0)
+        const lineItems=cart.map((item)=>({
+           price_Data:{
+           curency:"inr",
+            product_data:{
+                name:item.product.title,
+                images:[item.product.images[0].url],
+            },
+            unit_amount:Math.round(item.product.price*100),
+           } ,
+              quantity:item.quantity
+        }))
+        const session=await stripe.checkout.sessions.create({
+            payment_method_types:["card"],
+            line_items:lineItems,
+            mode:"payment",
+            success_url:`${process.env.Frontend_Url}/ordersuccess?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url:`${process.env.Frontend_Url}/cart`,
+            metadata:{
+                userId:req.user._id.toString(),
+                method,
+                phone,
+                address,
+                subTotal
+            }
+        })
+        res.json({url:session.url})
+       
+        
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({message:"newOrderOnline server error"})  
+    }
+}
+
+
+const verifyPayment=async(req,res)=>{
+    try {
+       const {sessionsId} = req.body
+         const session=await stripe.checkout.sessions.retrieve(sessionsId)
+       
+         const {userId,method,phone,address,subTotal}=session.metadata
+
+         const cart=await Cart.find({user:userId}).populate("products")
+
+         const items=cart.map((i)=>{
+            return{
+                product:i.product._id,
+                name:i.product.title,
+                price:i.product.price,
+                quantity:i.quantity
+            }
+         })
+
+         if(cart.length===0){
+            return res.status(400).json({message:"Cart is empty"})
+         }
+
+         const existingOrder=await Order.findOne({paymentInfo:sessionsId})
+
+            if(!existingOrder){
+               const order=await Order.create({
+                   items:cart.map((item)=>({
+                    product:item.product._id,
+                   quantity:item.quantity,
+                   })),
+                   method,
+                   phone,
+                   address,
+                   subTotal,
+                   paymentInfo:sessionsId,
+                   user:userId,
+                   paidAt:new Date()
+                })
+                for(let i of order.items){
+            const product=await Product.findById(i.product)
+
+            if(product){
+                product.stock-=i.quantity
+                product.sold+=i.quantity
+                await product.save()
+            }
+        }
+        await Cart.deleteMany({user:req.user._id})
+
+        await sendOrderConfirmation({email:req.user.email,subject:"Order Confirmation",orderId:order._id,
+            products:items,
+            totalAmount:subTotal
+        })
+        return res.json({message:"Payment verified and order placed successfully",order})
+
+ 
+            }
+
+
+
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message:"verifyPayment server error"})
+    }
+}
+
+
+module.exports={newOrderCode,getAllOrders,getAllOrdersAdmin,getMyOrder,updateStatus,getStats,newOrderOnline,verifyPayment}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,7 @@
 
 const express = require("express")
 const isAuth = require("../middlewares/isAuth")
-const { newOrderCode, getAllOrders, getAllOrdersAdmin, getMyOrder, updateStatus, getStats } = require("../controller/order")
+const { newOrderCode, getAllOrders, getAllOrdersAdmin, getMyOrder, updateStatus, getStats, newOrderOnline, verifyPayment } = require("../controller/order")
 
 const router = express.Router()
 
@@ -17,6 +17,11 @@ router.get("/order/:id",isAuth,getMyOrder)
 router.post("/order/:id",isAuth,updateStatus)
 
 router.get("/stats",isAuth,getStats)
+
+router.post("/order/new/online",isAuth,newOrderOnline)
+
+router.post("order/verify/payment",isAuth,verifyPayment)
+
 
 
 module.exports = router
